506e59178e2298e39a4bcc157b1afc3312df731d
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-28 12:04:18 +0530

    sign in controller

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -12,3 +12,19 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "user not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "invalid password"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
