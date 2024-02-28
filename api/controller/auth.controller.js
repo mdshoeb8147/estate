b0cc767dc23b5d65512b2a8f873a552b3e6b8772
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-28 12:17:55 +0530

    sign in route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -24,6 +25,16 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(401, "invalid password"));
     }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        // expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
