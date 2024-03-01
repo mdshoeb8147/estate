fe9508aac65ee238b12c54a2773f8bd70e028774
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-01 16:02:08 +0530

    delete api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -10,6 +10,7 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, "you can only delete your account"));
   try {
     await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access-token");
     res.status(200).json("user has been deleted");
   } catch (error) {
     next(error);
