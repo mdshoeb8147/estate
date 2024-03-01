a4febbf1898dc1dcd88e6417de9511fb4a587830
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-01 15:33:57 +0530

    delete api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,10 @@
+import { errorHandler } from "../utils/error";
+
 export const testRoute = (req, res) => {
   res.send("Hello welcome to test route!");
+};
+
+export const deleteUser = (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only delete your account"));
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { testRoute } from "../controller/user.controller.js";
+import { deleteUser, testRoute } from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", testRoute);
+router.delete("delete/:id", verifyToken, deleteUser);
 
 export default router;
