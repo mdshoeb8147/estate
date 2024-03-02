11b90511c3be046e6103094b5d7b71d8c5a955d6
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-02 11:49:20 +0530

    listing api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
@@ -20,13 +20,14 @@ app.listen(3000, () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth/", authRouter);
+app.use("api/listing", listingRouter);
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";
   return res.status(statusCode).json({
-    success:false;
-    statusCode;
-    message
-  })
-})
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ api/routes/listing.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { createListing } from "../controller/listing.controller";
+import { verifyToken } from "../utils/verifyUser.js";
+const router = express.Router();
+
+router.post("/create", verifyToken, createListing);
