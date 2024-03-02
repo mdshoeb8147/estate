af8a9cef11dbfe50fc5f76ffb7a973f3a923f50c
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-02 22:41:59 +0530

    userlisting api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const testRoute = (req, res) => {
   res.send("Hello welcome to test route!");
@@ -14,5 +15,16 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("user has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id == req.params.id) {
+    try {
+      const listing = await Listing.find({ useRef: req.params.id });
+      res.status(200).json(listing);
+    } catch (error) {}
+  } else {
+    return next(errorHandler(401, "you can only view your listing"));
   }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { deleteUser, testRoute } from "../controller/user.controller.js";
+import {
+  deleteUser,
+  testRoute,
+  getUserListing,
+} from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", testRoute);
 router.delete("delete/:id", verifyToken, deleteUser);
-
+router.get("/listing/:id", verifyToken, getUserListing);
 export default router;
