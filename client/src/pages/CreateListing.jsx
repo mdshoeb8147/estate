838b06501b319abaff7924dbb58dae9a0ad92f25
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-02 15:59:29 +0530

    upload image

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+    }
+  };
+  const storeImage = async () => {};
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Create a List</h1>
@@ -111,6 +124,7 @@ const CreateListing = () => {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full "
               type="file"
               id="images"
@@ -118,7 +132,11 @@ const CreateListing = () => {
               accept="image/*"
               required
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
               Upload
             </button>
           </div>
