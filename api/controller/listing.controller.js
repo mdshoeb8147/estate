10e8a0d0838dd1e3eb5bbf042842b1b0b9104d95
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-03 14:38:15 +0530

    delete listing

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -8,3 +8,19 @@ export const createListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(401, "listing not found"));
+  }
+  if (req.user.id !== listing.userRef()) {
+    return next(errorHandler(401, "you can delete your own listing"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createListing } from "../controller/listing.controller";
+import { createListing, deleteListing } from "../controller/listing.controller";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 export default router;
