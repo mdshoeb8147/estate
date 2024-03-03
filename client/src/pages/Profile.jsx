fcb313dc85d9228657e903dc234f3b1d70c5abbf
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-03 12:22:03 +0530

    listing completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   deleteUserFailure,
   deleteUserStart,
@@ -113,11 +114,37 @@ const Profile = () => {
       <p className="text-red-500 mt-5">
         {showListingError ? "Error Showing listings" : " "}
       </p>
-      {userListings &&
-        userListings.length > 0 &&
-        userListings.map((listing) => (
-          <div className="" key={listing._id}></div>
-        ))}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+              key={listing._id}
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="Listing Cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
